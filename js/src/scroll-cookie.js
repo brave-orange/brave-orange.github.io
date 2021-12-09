166cbe27734209c0e21a16620b3691cdc524f593
yongcheng <anjxwyc@outlook.com>
2021-12-09 23:46:54 +0800

    Site updated: 2021-12-09 23:46:53

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 08 2021 23:05:33 GMT+0800 (GMT+08:00)
+// build time:Thu Dec 09 2021 23:46:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
